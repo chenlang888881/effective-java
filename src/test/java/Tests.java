608bb777eeddf63583be847fa6323301d8e7ac46
chenlang888881 <chenlang888881@users.noreply.github.com>
2018-04-10 11:06:22 +0800

    test 文件提交

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -9,27 +9,40 @@ import java.util.Set;
  */
 public class Tests {
 
+    /**
+     * 1.考虑用静态工厂方法代替构造器
+     * 2.遇到多个构造器参数时要考虑用构建器
+     * 3.用私有构造器或枚举类强化单例
+     * 4.通过私有构造器强化不可实例化的能力
+     * 5.避免创建不必要的对象
+     * 6.消除过期的对象引用
+     * 7.避免使用终结方法
+     */
+
+    /**
+     * 1.覆盖equals时请遵守通用约定
+     * 2.覆盖equals时总要覆盖hashcode
+     * 3.始终要覆盖toString
+     * 4.谨慎地覆盖clone方法
+     * 5.考虑实现comparable接口
+     */
+
+
+    /**
+     * 1.使类和成员的可访问性最小化
+     * 2.在公有类中使用访问方法而非共有域
+     * 3.使可变性最小化
+     * 4.复合优先于继承
+     * 5.要么为继承而设计，并提供文档说明，要么禁止用继承
+     * 6.接口优于抽象类
+     * 7.接口只用于定义类型
+     * 8.类层次优于标签类
+     * 9.函数对象表示策略
+     * 10.优先考虑静态成员类
+     */
     @Test
     public void test(){
-    /*    Integer integer = new Integer(1);
-        integer.*/
         Set<Student> set = new HashSet();
-        Student s1 = new Student();
-        s1.setName("bbb");
-        s1.setAge(111);
-        Student s2 = new Student();
-        s2.setName("bbb");
-        s2.setAge(111);
-        set.add(s1);
-        set.add(s2);
-        System.out.println(s1.equals(s2));
-        for (Student item:set){
-            System.out.println(item);
-        }
-        s1.setName("bbb");
-        System.out.println(s1.equals(s2));
-        for (Student item:set){
-            System.out.println(item);
-        }
+
     }
 }
